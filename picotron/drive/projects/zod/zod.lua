d57b93069703a4271991f049b92b8447c948e567
Anders Kjær Damgaard <akd@akd.io>
2025-05-07 22:40:04 +0200

    Extract parsers from zod-functions

--- picotron/drive/projects/zod/zod.lua
+++ picotron/drive/projects/zod/zod.lua
@@ -16,60 +16,64 @@
      this. Maybe it'll be validate, not parse, after all.
 ]]
 
-local zod = {}
-
-function zod.string()
-  local function parse(value, safe)
-    if type(value) != "string" then
-      local errorMessage = "Expected string, got " .. type(value)
-      return safe
-          and { success = false, error = errorMessage }
-          or error(errorMessage)
-    end
-
+local function parseString(value, safe)
+  if type(value) != "string" then
+    local errorMessage = "Expected string, got " .. type(value)
     return safe
-        and { success = true, data = value }
-        or value
+        and { success = false, error = errorMessage }
+        or error(errorMessage)
   end
 
+  return safe
+      and { success = true, data = value }
+      or value
+end
+
+local function zodString()
   return {
-    parse = function(value) return parse(value, false) end,
-    safeParse = function(value) return parse(value, true) end
+    parse = function(value) return parseString(value, false) end,
+    safeParse = function(value) return parseString(value, true) end
   }
 end
 
-function zod.object(schema)
-  local function parse(value, safe)
-    if type(value) ~= "table" then
-      local errorMessage = "Expected table, got " .. type(value)
-      return safe
-          and { success = false, error = errorMessage }
-          or error(errorMessage)
-    end
+local function parseObject(childSchemas, value, safe)
+  if type(value) ~= "table" then
+    local errorMessage = "Expected table, got " .. type(value)
+    return safe
+        and { success = false, error = errorMessage }
+        or error(errorMessage)
+  end
 
-    for key, fieldValidator in pairs(schema) do
-      if safe then
-        local result = fieldValidator.safeParse(value[key])
-        if not result.success then
-          return {
-            success = false,
-            error = "Error in field '" .. key .. "': " .. result.error
-          }
-        end
-      else
-        fieldValidator.parse(value[key])
+  for key, fieldValidator in pairs(childSchemas) do
+    if safe then
+      local result = fieldValidator.safeParse(value[key])
+      if not result.success then
+        return {
+          success = false,
+          error = "Error in field '" .. key .. "': " .. result.error
+        }
       end
+    else
+      fieldValidator.parse(value[key])
     end
-
-    return safe
-        and { success = true, data = value }
-        or value
   end
 
+  return safe
+      and { success = true, data = value }
+      or value
+end
+
+local function zodObject(childSchemas)
   return {
-    parse = function(value) return parse(value, false) end,
-    safeParse = function(value) return parse(value, true) end
+    parse = function(value) return parseObject(childSchemas, value, false) end,
+    safeParse = function(value) return parseObject(childSchemas, value, true) end
   }
 end
+
+
+local zod = {
+  string = zodString,
+  object = zodObject
+}
 
 return zod
